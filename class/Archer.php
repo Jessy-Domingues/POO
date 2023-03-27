f13d8af86ab33b322ed6bed6cb9eb1cbd7a526a9
Jessy-Domingues <jess4960@gmail.com>
2023-03-27 14:13:48 +0200

    autoloader

---
+++ class/Archer.php
@@ -0,0 +1,15 @@
+<?php
+class Archer extends Personnage{
+
+    public $vie = 40;
+
+    public function attaque($cible){
+        // attaquant c'est $this
+        // défenseur c'est $cible
+        $cible->vie -= $this->atk;
+        parent::attaque($cible);
+        $cible->empecher_negatif();
+    }
+
+
+}
